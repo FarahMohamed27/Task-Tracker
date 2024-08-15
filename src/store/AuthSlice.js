3f633858000b9a0c67a576cf83e83fbd2718b786
FarahMohamed27 <105306483+FarahMohamed27@users.noreply.github.com>
2024-08-16 00:44:33 +0300

    Add AuthSlice file

---
+++ src/store/AuthSlice.js
@@ -0,0 +1,28 @@
+import { createSlice } from "@reduxjs/toolkit";
+
+const AuthSlice = createSlice({
+    name: 'auth',
+    initialState: {
+        email: '',
+        userName: '',
+        isAuthenticated: false,
+        phone: ''
+    },
+    reducers: {
+        login(state, action){
+            state.email = action.payload.email;
+            state.userName = action.payload.userName;
+            state.isAuthenticated = true;
+            state.phone = action.payload.phone
+        },
+        logout(state){
+            state.email = '';
+            state.userName = '';
+            state.isAuthenticated = false;
+            state.phone = '';
+        },
+    }
+})
+
+export const authActions = AuthSlice.actions;
+export default AuthSlice.reducer;
